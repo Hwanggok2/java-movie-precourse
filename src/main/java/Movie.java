b29e8d6d55a84c4bcbce65898259a9c8ad25c621
junu02 <89693764+Hwanggok2@users.noreply.github.com>
2025-11-09 17:03:07 +0900

    Feat(Movie): 영화 객체 구현

---
+++ src/main/java/Movie.java
@@ -0,0 +1,17 @@
+public class Movie {
+    private final String title;
+    private final int runningTime;
+
+    public Movie(String title, int runningTime) {
+        this.title = title;
+        this.runningTime = runningTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }   
+
+    public int getRunningTime() {
+        return runningTime;
+    }
+}
