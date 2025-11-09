39bf28a725681cb56995c6105bafb970deeb6c47
junu02 <89693764+Hwanggok2@users.noreply.github.com>
2025-11-09 16:54:09 +0900

    Refactor : Column 수정

--- src/main/java/Column.java
+++ src/main/java/Column.java
@@ -1,3 +1,13 @@
 public enum Column {
-    ONE, TWO, THREE, FOUR, FIVE;
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5);
+    
+    private final int number;
+    
+    Column(int number) {
+        this.number = number;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
 }
