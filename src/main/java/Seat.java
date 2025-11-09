ad606ba7d914cd5f3cee267b26c80533c2002841
junu02 <89693764+Hwanggok2@users.noreply.github.com>
2025-11-09 17:00:36 +0900

    Feat(Seat): Seat 객체 생성

---
+++ src/main/java/Seat.java
@@ -0,0 +1,17 @@
+public class Seat {
+    private final SeatNumber seatNumber;
+    private final SeatGrade seatGrade;
+
+    public Seat(SeatNumber seatNumber, SeatGrade seatGrade) {
+        this.seatNumber = seatNumber;
+        this.seatGrade = seatGrade;
+    }
+
+    public SeatNumber getSeatNumber() {
+        return seatNumber;
+    }
+
+    public SeatGrade getSeatGrade() {
+        return seatGrade;
+    }
+}
